9cb3d9b5d56a56fc04933c9992e2c47befe3c0bf
@mathbelga013 <matheusgomes110901@gmail.com>
2025-08-18 16:59:45 -0300

    aplicando conceitos de encapsulamentos na classe "usuario".

--- lib/usuario.dart
+++ lib/usuario.dart
@@ -1,13 +1,32 @@
 class Usuario{
-  int? codigo;
-  String? nome;
-  String? login;
-  String? senha;
+  int? _codigo;
+  String? _nome;
+  String? _login;
+  String? _senha;
 
-  Usuario({
-    this.nome,
-    this.codigo,
-    this.senha,
-    this.login
-  });
+  Usuario({String? nome, int? codigo, String? senha, String? login,}) {
+    _senha = senha;
+    _codigo = codigo;
+    _nome = nome;
+    _login = login;
+  }
+   //getters
+  // escrita do get tradicional
+ String? getNome() {
+   return _nome;
+  }
+  //escrita do get no estilo dart
+String? get nome => _nome;
+String? get senha => _senha;
+int? get codigo => _codigo;
+
+  //setters
+ //escrita dos set tradicional
+ set login (String login){
+   _login = login;
+ }
+ //escrita do set no estilo Dart
+ set nome(String? nome) => _nome = nome;
+ set senha(String? senha) => _senha = senha;
+ set codigo(int? codigo) => _codigo = codigo;
 }
