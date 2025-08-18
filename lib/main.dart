36e03fe393b778484f54e6746e358e06e72c1390
@mathbelga013 <matheusgomes110901@gmail.com>
2025-08-18 16:10:38 -0300

    Criando projeto e classe "usuario".

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'usuario.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    Usuario u = new Usuario(
+      codigo: 1,
+      login: "Matheus013",
+      nome: "Matheus",
+      senha: "@senhaforte013"
+    );
+    return MaterialApp(
+       home: Scaffold(
+         appBar: AppBar(
+           title: const Text("Meus Dados:")
+         ),
+         body: Center(
+           child: Padding(padding: const EdgeInsets.all(18.0),
+           child: Column(
+             children: [
+               Text("Nome: ${u.nome} "),
+               Text("codigo: ${u.codigo}"),
+               Text("login: ${u.login}"),
+               Text("senha: ${u.senha}"),
+             ],
+           ),
+           ),
+         ),
+       ),
+       );
+  }
+}
